c3544d528c654022423fc79855f44aad2da8b9ca
nicco b <niccobeats@icloud.com>
2022-07-02 07:08:16 -0700

    s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	env: {
+		NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+	},
 }
 
 module.exports = nextConfig
