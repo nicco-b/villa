3a1ce9edb47e07f5f78f92a35dc94c6a556c7c0f
nicco b <niccobeats@icloud.com>
2022-07-04 16:29:08 -0700

    l

--- pages/api/checkout_sessions.js
+++ pages/api/checkout_sessions.js
@@ -7,7 +7,8 @@ const inventory = [
 	{
 		name: 'Bananas',
 		description: 'Yummy yellow fruit',
-		id: 'sku_GBJ2Ep8246qeeT',
+		id: 'poop',
+
 		price: 400,
 		image:
 			'https://images.unsplash.com/photo-1574226516831-e1dff420e562?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=225&q=80',
@@ -16,8 +17,8 @@ const inventory = [
 	},
 	{
 		name: 'Tangerines',
-		id: 'sku_GBJ2WWfMaGNC2Z',
-		price: 100,
+		id: 'price_1LHMVIHFFw3ZIEqv4zXNFN4k',
+
 		image:
 			'https://images.unsplash.com/photo-1482012792084-a0c3725f289f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=225&q=80',
 		attribution: 'Photo by Jonathan Pielmayer on Unsplash',
@@ -28,29 +29,15 @@ export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		console.log('ur here')
 		try {
-			// const line_items = await validateCartItems(
-			// 	inventory,
-			// 	{
-			// 		name: 'Tangerines',
-			// 		id: 'sku_GBJ2WWfMaGNC2Z',
-			// 		price: 100,
-			// 		image:
-			// 			'https://images.unsplash.com/photo-1482012792084-a0c3725f289f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=225&q=80',
-			// 		attribution: 'Photo by Jonathan Pielmayer on Unsplash',
-			// 		currency: 'USD',
-			// 	},
+			const line_items = await validateCartItems(
+				inventory,
 
-			// 	{
-			// 		name: 'Bananas',
-			// 		description: 'Yummy yellow fruit',
-			// 		id: 'sku_GBJ2Ep8246qeeT',
-			// 		price: 400,
-			// 		image:
-			// 			'https://images.unsplash.com/photo-1574226516831-e1dff420e562?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=225&q=80',
-			// 		attribution: 'Photo by Priscilla Du Preez on Unsplash',
-			// 		currency: 'USD',
-			// 	}
-			// )
+				{
+					poop: {
+						quantity: 1,
+					},
+				}
+			)
 			const params = {
 				submit_type: 'pay',
 				payment_method_types: ['card'],
@@ -58,14 +45,9 @@ export default async function handler(req, res) {
 				shipping_address_collection: {
 					allowed_countries: ['US', 'CA'],
 				},
-				line_items: [
-					{
-						quantity: 1,
-						price: 'price_1LHMVIHFFw3ZIEqv4zXNFN4k',
-					},
-				],
-				success_url: `http://localhost:3000/result?session_id={CHECKOUT_SESSION_ID}`,
-				cancel_url: `http://localhost:3000/`,
+				line_items,
+				success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+				cancel_url: `${req.headers.origin}/`,
 				mode: 'payment',
 			}
 			// Create Checkout Sessions from body params.
