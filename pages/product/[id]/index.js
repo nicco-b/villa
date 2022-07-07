89cacaf532f5a6b3bec109aba9e1a44843814548
nicco b <niccobeats@icloud.com>
2022-07-07 13:51:28 -0700

    2 min

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -71,7 +71,7 @@ export async function getStaticProps(context) {
 		// Next.js will attempt to re-generate the page:
 		// - When a request comes in
 		// - At most once every 10 seconds
-		revalidate: 10, // In seconds
+		revalidate: 120, // In seconds
 	}
 }
 export async function getStaticPaths(context) {
