79874a65140a33adb6b5e7eee2412844f56f4c76
nicco b <niccobeats@icloud.com>
2022-07-07 11:25:50 -0700

    HEHE

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -22,7 +22,7 @@ router.post(async (req, res) => {
 	console.log('why', req.rawBody)
 	const stripe = initStripe(process.env.STRIPE_SECRET_KEY)
 	const signature = req.headers['stripe-signature']
-	const signingSecret = 'we_1LIynSHFFw3ZIEqvLKBCfU7m'
+	const signingSecret = process.env.STRIPE_WHSEC
 	const reqBuffer = await buffer(req)
 	let event
 
