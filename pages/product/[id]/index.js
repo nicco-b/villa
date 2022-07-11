fd7576b5a618157264f6cb33231541507bbaef97
nicco b <niccobeats@icloud.com>
2022-07-11 16:55:20 -0700

    ?

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -64,7 +64,11 @@ export async function getStaticPaths() {
 	// We'll pre-render only these paths at build time.
 	// { fallback: blocking } will server-render pages
 	// on-demand if the path doesn't exist.
-	return { paths, fallback: false }
+	return {
+		paths,
+		fallback: false,
+		revalidate: 1,
+	}
 }
 
 export async function getStaticProps({ params }) {
