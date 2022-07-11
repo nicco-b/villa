bf383c4bf547563fd62ea8f22bf6b9b2d729e844
nicco b <niccobeats@icloud.com>
2022-07-11 14:20:02 -0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
 	// )
 
 	const products = await getProducts()
-
+	console.log({ products })
 	// Pass data to the page via props
 	return {
 		props: { products: products },
