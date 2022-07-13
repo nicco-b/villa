1851d133e0b3d49770f70bf67fb8dcecdea4378b
nicco b <niccobeats@icloud.com>
2022-07-12 17:14:51 -0700

    l

--- pages/product/[id]/index.js
+++ pages/product/[id]/index.js
@@ -72,7 +72,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
 	console.log('running getStaticProps, params', params)
-	const product = await getProductById(params.slug)
+	const product = await getProductById(params.id)
 	console.log(product)
 	return {
 		props: {
