1f3e23c367bf29d1ab5446f3adbb8bfc41caed4a
nicco b <niccobeats@icloud.com>
2022-07-18 15:31:30 -0700

    update images

--- components/topbar/index.js
+++ components/topbar/index.js
@@ -15,25 +15,39 @@ export const TopBar = ({ animate }) => {
 					style={{
 						display: 'flex',
 						alignItems: 'center',
-						maxWidth: '200px',
 					}}>
 					<div className={styles.totemBox}>
-						<Image src='/totem1.png' alt='totem1' height={40} width={40} />
+						<Image src='/totems/totem1.png' alt='totem1' height={40} width={40} />
 					</div>
 					<div className={styles.totemBox}>
-						<Image src='/totem3.png' alt='totem2' height={40} width={40} />
+						<Image src='/totems/totem3.png' alt='totem2' height={40} width={40} />
 					</div>
 					<div className={styles.totemBox}>
-						<Image src='/totem2.png' alt='totem3' height={40} width={40} />
+						<Image src='/totems/totem2.png' alt='totem3' height={40} width={40} />
 					</div>
 					<div className={styles.totemBox}>
-						<Image src='/totem4.png' alt='totem4' height={40} width={40} />
+						<Image src='/totems/totem4.png' alt='totem4' height={40} width={40} />
 					</div>
 					<div className={styles.totemBox}>
-						<Image src='/totem5.png' alt='totem5' height={40} width={40} />
+						<Image src='/totems/totem5.png' alt='totem5' height={40} width={40} />
 					</div>
 					<div className={styles.totemBox}>
-						<Image src='/totem6.png' alt='totem6' height={40} width={40} />
+						<Image src='/totems/totem6.png' alt='totem6' height={40} width={40} />
+					</div>
+					<div className={styles.totemBox}>
+						<Image src='/totems/fishtotem.png' alt='totem6' height={40} width={40} />
+					</div>
+					<div className={styles.totemBox}>
+						<Image src='/totems/firetotem.png' alt='totem6' height={40} width={40} />
+					</div>
+					<div className={styles.totemBox}>
+						<Image src='/totems/housetotem.png' alt='totem6' height={40} width={40} />
+					</div>
+					<div className={styles.totemBox}>
+						<Image src='/totems/boottotem.png' alt='totem6' height={40} width={40} />
+					</div>
+					<div className={styles.totemBox}>
+						<Image src='/totems/cloudtotem.png' alt='totem6' height={40} width={40} />
 					</div>
 				</div>
 			</div>
