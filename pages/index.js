4e702e58ee3b35254ff68e1984ad0005191f915f
nicco b <niccobeats@icloud.com>
2022-07-29 07:45:11 -0700

    qtf

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { SWRConfig } from 'swr'
 import MainLayout from '../components/layouts/MainLayout'
-import styles from '../styles/Home.module.css'
 import { getProducts } from './api/products/products'
 import { getScheduledSales } from './api/schedule'
 import Home from './home'
@@ -25,16 +22,8 @@ Index.getLayout = function getLayout(page) {
 	return <MainLayout>{page}</MainLayout>
 }
 export async function getStaticProps() {
-	// Fetch data from external API
-	// const dev = process.env.NODE_ENV !== 'production'
-
-	// const res = await fetch(
-	// 	`${dev ? 'http://' : 'https://'}${process.env.NEXT_PUBLIC_VERCEL_URL}/api/products/products`
-	// )
-	//first check scheduled_sales
 	const scheduled_sales = await getScheduledSales()
 	const products = await getProducts()
-	// console.log('ss', scheduled_sales?.included_products)
 	const ss = JSON.parse(JSON.stringify(scheduled_sales))
 
 	if (scheduled_sales.length > 0) {
@@ -48,14 +37,11 @@ export async function getStaticProps() {
 		}
 	}
 
-	// console.log({ products })
-	// Pass data to the page via props
 	return {
 		props: {
 			fallback: {
 				'/api/products/products': products,
 			},
 		},
-		// In seconds
 	}
 }
