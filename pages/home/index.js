9b2636013cdf565451779b6cb05ec3e36e7979e9
nicco b <niccobeats@icloud.com>
2022-07-29 08:52:15 -0700

    s

--- pages/home/index.js
+++ pages/home/index.js
@@ -69,12 +69,12 @@ export default function Home() {
 													sale={sale}
 												/>
 											</div>
-											{sale?.included_products?.length > 0 && (
+											{
 												<Products
 													products={sale?.included_products?.length > 0 ? sale.included_products : []}
 													isValidating={isValidating}
 												/>
-											)}
+											}
 										</>
 									)
 								})}
