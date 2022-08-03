b00c60ebbbde07c2eb066c714dbaa3b2245c14d1
nicco b <niccobeats@icloud.com>
2022-08-02 20:48:39 -0700

    cute

--- pages/home/Countdown.js
+++ pages/home/Countdown.js
@@ -1,4 +1,4 @@
-import moment from 'moment'
+import moment, { min } from 'moment'
 import { tz } from 'moment-timezone'
 import { useEffect } from 'react'
 import { useCountdown } from '../../hooks/useCountdown'
@@ -28,15 +28,15 @@ export const CountdownTimer = ({ date, setCountdownFinished, sale }) => {
 		)
 	}
 }
-const DateTimeDisplay = ({ value, type, isDanger }) => {
+const DateTimeDisplay = ({ value, type, isDanger, color }) => {
 	return (
 		<div className={isDanger ? 'countdown danger' : 'countdown'}>
 			<p
 				style={{
 					fontSize: '2rem',
-					color: value === 0 ? '#36966280' : '#369662',
+					color: color,
 				}}>
-				{value.toLocaleString('en-US', { minimumIntegerDigits: 2 })}
+				{value?.toLocaleString('en-US', { minimumIntegerDigits: 2 })}
 			</p>
 		</div>
 	)
@@ -68,11 +68,39 @@ const ShowCounter = ({ days, hours, minutes, seconds, sale }) => {
 				display: 'grid',
 				justifyItems: 'center',
 				justifyContent: 'center',
-				gap: '1rem',
+				gap: '0.5rem',
 				color: '#369662',
 				padding: '3em 0 0 0',
+				userSelect: 'none',
 			}}>
-			<div>{sale.callout}</div>
+			<div
+				style={{
+					fontSize: '1.23rem',
+					fontWeight: '600',
+					color: '#369662',
+				}}>
+				{sale.callout}
+			</div>
+			<div
+				style={{
+					fontSize: '1.1rem',
+					fontWeight: '450',
+					color: '#36966290',
+					padding: '0em 0 1em 0',
+				}}>
+				{/* {new Date(sale.start_date).toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles' })} */}
+				{/* {new Date(sale.start_date).toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' })} */}
+				{/* {new Date(sale.start_date).getTimezoneOffset() === 420 ? 'PDT' : 'PST'} */}
+				{mDate.tz('America/Los_Angeles').format('LLL')}{' '}
+				<span
+					style={{
+						fontSize: '1.1rem',
+						fontWeight: '600',
+						color: '#36966290',
+					}}>
+					{mDate.tz('America/Los_Angeles').format('z')}
+				</span>
+			</div>
 			<div
 				className='show-counter'
 				style={{
@@ -89,38 +117,55 @@ const ShowCounter = ({ days, hours, minutes, seconds, sale }) => {
 						gap: '2px',
 						width: '250px',
 					}}>
-					<DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
+					<DateTimeDisplay
+						color={days === 0 ? '#36966280' : '#369662'}
+						value={days}
+						type={'Days'}
+						isDanger={days <= 3}
+					/>
 					<p
 						style={{
-							color: '#369662',
+							color: days === 0 ? '#36966280' : '#369662',
+
 							fontSize: '2rem',
 						}}>
 						:
 					</p>
-					<DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
+					<DateTimeDisplay
+						color={hours === 0 ? '#36966280' : '#369662'}
+						value={hours}
+						type={'Hours'}
+						isDanger={false}
+					/>
 					<p
 						style={{
-							color: '#369662',
+							color: hours === 0 ? '#36966280' : '#369662',
+
 							fontSize: '2rem',
 						}}>
 						:
 					</p>
-					<DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
+					<DateTimeDisplay
+						color={hours === 0 && minutes === 0 ? '#36966280' : '#369662'}
+						value={minutes}
+						type={'Mins'}
+						isDanger={false}
+					/>
 					<p
 						style={{
-							color: '#369662',
+							color: minutes === 0 ? '#36966280' : '#369662',
+
 							fontSize: '2rem',
 						}}>
 						:
 					</p>
-					<DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
+					<DateTimeDisplay
+						color={minutes === 0 && seconds === 0 ? '#36966280' : '#369662'}
+						value={seconds}
+						type={'Seconds'}
+						isDanger={false}
+					/>
 				</div>
-			</div>
-			<div>
-				{/* {new Date(sale.start_date).toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles' })} */}
-				{/* {new Date(sale.start_date).toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' })} */}
-				{/* {new Date(sale.start_date).getTimezoneOffset() === 420 ? 'PDT' : 'PST'} */}
-				{mDate.tz('America/Los_Angeles').format('LLL z')}
 			</div>
 		</div>
 	)
