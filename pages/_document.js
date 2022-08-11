0eee3a9ab5ec7da5adaec1aa3631c524760625b8
nicco b <niccobeats@icloud.com>
2022-08-10 22:04:47 -0700

    inter from google

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,11 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
 					<link
-						rel='preload'
-						href='https://assets.niccob.com/assets/fonts/Inter/Inter.var.woff2'
-						as='font'
-						crossOrigin=''
-						type='font/woff2'
+						href='https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap'
+						rel='stylesheet'
 					/>
 				</Head>
 				<body>
