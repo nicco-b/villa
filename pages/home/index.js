fc134d4478350bd703ad4095ddf319a4befd8f1b
nicco b <niccobeats@icloud.com>
2022-08-13 10:18:28 -0700

    ?

--- pages/home/index.js
+++ pages/home/index.js
@@ -39,6 +39,7 @@ export default function Home() {
 				<div className={styles.main}>
 					<div
 						style={{
+							width: '100%',
 							height: '100%',
 							display: 'grid',
 							padding: '1em',
